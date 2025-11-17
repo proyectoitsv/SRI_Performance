a2d95ae7569e4f3cfaad1aa446d1fb199477102d
NachoBern2457 <ignaciobernatmaggi@gmail.com>
2025-11-17 18:58:28 -0300

    Restructure project and migrate source files
    Moved source files from Code/ to Software/, updated .gitignore, added PlatformIO configuration, and removed build artifacts and VSCode settings. This change organizes the project structure for better maintainability and prepares for future development in the Software directory.

--- Code/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/ignac/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/ignac/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/ignac/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/ignac/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
